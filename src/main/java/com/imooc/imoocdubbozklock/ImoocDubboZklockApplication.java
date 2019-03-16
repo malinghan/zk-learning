ffad7a33949859c5975f5c73a7f24f577384e498
马凌汉 <malinghan@qijiayoudao.com>
2019-03-16 12:49:38 +0800

    uii

---
+++ src/main/java/com/imooc/imoocdubbozklock/ImoocDubboZklockApplication.java
@@ -0,0 +1,13 @@
+package com.imooc.imoocdubbozklock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImoocDubboZklockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImoocDubboZklockApplication.class, args);
+    }
+
+}
